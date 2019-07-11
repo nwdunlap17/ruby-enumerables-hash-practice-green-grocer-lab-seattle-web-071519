8cf234d7dc985be79bfb51d88f454973fba926d4
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 21:31:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ def checkout(cart, coupons)
   cart =apply_coupons(cart,coupons)
   p cart = apply_clearance(cart)
   
-  total = 0
+  total = 1
   cart.each do |item|
     total += item[1][:price]
   end

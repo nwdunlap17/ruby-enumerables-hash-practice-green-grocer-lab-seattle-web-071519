0060472b4eb4e951a3bcde931063169cee3e3ac8
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 20:52:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,10 +30,12 @@ cart = [
 #      {"KALE" => {:price => 3.00, :clearance => false}},
 #      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
 #      {"ALMONDS" => {:price => 9.00, :clearance => false}},
-      {"TEMPEH" => {:price => 3.00, :clearance => true}},
-      {"CHEESE" => {:price => 6.50, :clearance => false}},
-      {"BEER" => {:price => 13.00, :clearance => false}},
-      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
-      {"BEETS" => {:price => 2.50, :clearance => false}},
-      {"SOY MILK" => {:price => 4.50, :clearance => true}}
+#      {"TEMPEH" => {:price => 3.00, :clearance => true}},
+#      {"CHEESE" => {:price => 6.50, :clearance => false}},
+#      {"BEER" => {:price => 13.00, :clearance => false}},
+#      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+#      {"BEETS" => {:price => 2.50, :clearance => false}},
+#      {"SOY MILK" => {:price => 4.50, :clearance => true}}
     ]
+    
+puts consolidate_cart(cart)

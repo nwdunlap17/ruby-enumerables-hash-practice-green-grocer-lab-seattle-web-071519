337485dacbcd198f0da5c96234427c4a6d0598a7
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 20:51:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,12 @@
 def consolidate_cart(cart)
   checkoutLine = {}
-  cart.each do |item|
-    if checkoutLine[item] != nil 
-      checkoutLine.item = item
+  cart.size.times do |i|
+    item = cart[i].keys[0]
+    if checkoutLine[item] === nil 
+      checkoutLine[item] = {:price => 0, :clearance => false}
     end
-  end   
+  end
+  return checkoutLine
 end
 
 def apply_coupons(cart, coupons)
@@ -22,3 +24,16 @@ def checkout(cart, coupons)
   end
   return sum
 end
+
+cart = [
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
+#      {"KALE" => {:price => 3.00, :clearance => false}},
+#      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
+#      {"ALMONDS" => {:price => 9.00, :clearance => false}},
+      {"TEMPEH" => {:price => 3.00, :clearance => true}},
+      {"CHEESE" => {:price => 6.50, :clearance => false}},
+      {"BEER" => {:price => 13.00, :clearance => false}},
+      {"PEANUTBUTTER" => {:price => 3.00, :clearance => true}},
+      {"BEETS" => {:price => 2.50, :clearance => false}},
+      {"SOY MILK" => {:price => 4.50, :clearance => true}}
+    ]

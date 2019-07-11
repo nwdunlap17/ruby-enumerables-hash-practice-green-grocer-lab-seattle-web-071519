2d8e9424025f41ecb1010dad5c8356b25f29e505
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 21:19:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,7 @@ cart = [
       {"AVOCADO" => {:price => 3.00, :clearance => true}},
       {"AVOCADO" => {:price => 3.00, :clearance => true}},
       {"AVOCADO" => {:price => 3.00, :clearance => true}},
-      {"AVOCADO" => {:price => 3.00, :clearance => true}}
+      {"AVOCADO" => {:price => 3.00, :clearance => true}},
       {"KALE" => {:price => 3.00, :clearance => false}},
 #      {"BLACK_BEANS" => {:price => 2.50, :clearance => false}},
 #      {"ALMONDS" => {:price => 9.00, :clearance => false}},

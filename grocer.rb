bec5af6820522ab271a64bd2d7a2c5104e65fab4
Nicholas Dunlap <pjnick-300@hotmail.com>
2019-07-11 21:32:19 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -72,7 +72,7 @@ cart = [
     
 coupons = [
       {:item => "AVOCADO", :num => 2, :cost => 5.00},
-      {:item => "AVOCADO", :num => 2, :cost => 5.00}
+      #{:item => "AVOCADO", :num => 2, :cost => 5.00}
       #{:item => "BEER", :num => 2, :cost => 20.00},
       #{:item => "CHEESE", :num => 3, :cost => 15.00}
     ]    
